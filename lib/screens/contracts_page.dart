83400f1c1c29bf428cf5dd4b27bb21d5b9c103f8
theEmris <emirsmorganson2000@gmail.com>
2022-04-09 17:12:47 +0500

    pages are added

--- lib/screens/contracts_page.dart
+++ lib/screens/contracts_page.dart
@@ -1,3 +1,4 @@
+import 'package:calendar_agenda/calendar_agenda.dart';
 import 'package:flutter/material.dart';
 import 'package:yiqilma_app/widgets/myAppBars.dart';
 
@@ -15,18 +16,44 @@ class ContractsPage extends StatelessWidget {
         flex: 2,
         child: Container(
           color: Color(0xFF1E1E20),
+
+child:CalendarAgenda(
+    backgroundColor: Colors.transparent,
+    selectedDateColor:Color(0xFF00A795),
+    dateColor: Color(0xFFD1D1D1),
+    initialDate: DateTime.now(),
+    firstDate: DateTime.now().subtract(Duration(days: 140)),
+    lastDate: DateTime.now().add(Duration(days: 4)),
+    onDateSelected: (date) {
+    print(date);
+},
+)
+
+
+
         )),
 
       Expanded(
         flex: 1,
-        child: Container(
-        color: Colors.greenAccent,
-      )),
+        child:  TabBar(
+        tabs: [
+          Tab(icon: Icon(Icons.flight)),
+          Tab(icon: Icon(Icons.directions_transit)),
+          Tab(icon: Icon(Icons.directions_car)),
+        ],
+        
+      ),
 
+      ),
       Expanded(
         flex: 7,
         child: Container(
           color: Colors.amber,
+          child: TabBarView(children: [
+            Icon(Icons.flight, size: 350),
+        Icon(Icons.directions_transit, size: 350),
+        Icon(Icons.directions_car, size: 350),
+          ]),
       ))
 
     ],),
